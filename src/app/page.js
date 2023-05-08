5ca8a771e4f8a939d25705ea990849f4c4c47663
tgoodall3 <Tylergoodall3@gmail.com>
2023-05-08 19:47:53 -0400

    page

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,9 @@ export default function Home() {
    <main className='bg-white dark:bg-violet-900'> 
     <section className='p-5'>
       <nav className='flex justify-between mb-20'>
-        <h1 className='mt-5 text-2xl font-burtons dark:text-violet-400'>DevelopedByTyler</h1>
+        <h1 className='mt-5 text-2xl font-burtons text-violet-800 dark:text-violet-400'>DevelopedByTyler</h1>
         <ul className='flex items-center'>
-          <li><BsFillMoonStarsFill onClick={() => SetDarkMode(!darkMode)} className='cursor-pointer text-xl font-burtons  '/></li>
+          <li><BsFillMoonStarsFill onClick={() => SetDarkMode(!darkMode)} className='cursor-pointer text-xl font-burtons dark:text-white  '/></li>
           <li><a className=' bg-gradient-to-r from-violet-800 to-violet-400 px-5 py-2 rounded-md ml-8 text-fuchsia-50 drop-shadow-xl' href='#'>Resume</a></li>
         </ul>
       </nav>
@@ -31,13 +31,15 @@ export default function Home() {
       <AiFillLinkedin className='cursor-pointer'/>
       <AiFillYoutube className='cursor-pointer'/>
       </div>
-      <div className='relative bg-gradient-to-b from-violet-400 to-white rounded-full w-80 h-80 mt-20 mx-auto overflow-hidden '>
+      <div className='relative bg-gradient-to-b from-violet-400 to-white rounded-full w-80 h-80 mt-10 mb-10 mx-auto overflow-hidden '>
         <Image src={avatar} layout='fill' object-objectFit='cover' />
       </div>
     </section>
 
-    <section>
-      <div>
+    <section className='p-5 px-20 flex justify-center bg-violet-400 dark:bg-violet-800'>
+      <div className=''>
+        <h3 className='font-vina text-white text-3xl font-md mt-20 tracking-wide '>What I specialize in!</h3>
+        <p className='font-semibold text-lg dark:text-violet-200'>During my time in college, I've spent most my time learning html, css, and js. I've also worked with Firebase and SQL. Ive more recently dived into React and working making CRUD applications. My passion has continued to grow and I use my free time perfecting my craft.</p>
       </div>
     </section>
     </main>
